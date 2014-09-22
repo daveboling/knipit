840f3788622ef6eb6b2247efda6f177f4a2004ab
David D. Boling <daviddboling@gmail.com>
2014-09-22 14:52:51 -0500

    Added Deck.getDecks() for when user logs into control panel

--- client/views/user-page/user-home.js
+++ client/views/user-page/user-home.js
@@ -4,21 +4,38 @@
   var userHome = angular.module('knipit');
 
   //CONTROLLER
-  userHome.controller('UserHomeCtrl', ['$scope', 'LocalUser', function($scope, LocalUser){
+  userHome.controller('UserHomeCtrl', ['$scope', '$rootScope', 'LocalUser', function($scope, $rootScope, LocalUser){
     $scope.title = 'User-Page';
     $scope.deck = {};
     $scope.decks = [];
 
+    console.log($rootScope.user.email || 'No email');
+
     function fail(){
-      toastr.error('You have failed me for the last time.');
+      toastr.error('Something went wrong.');
     }
 
+    $scope.getDecks = function(){
+      LocalUser.getDecks($rootScope.user.email).then(function(res){
+        $scope.decks = res.data.decks;
+      }, fail);
+    };
 
     $scope.createDeck = function(){
       LocalUser.createDeck($scope.deck).then(function(res){
         $scope.decks.push(res.data.deck);
+        $scope.deck = {};
+        $scope.new = false;
       }, fail);
     };
+
+    //start flip
+
+    //start quiz
+
+    //edit deck
+
+    //delete deck
   }]);
 
   //FACTORY
@@ -28,10 +45,14 @@
       return $http.post('/deck/create', deck);
     }
 
-    return {createDeck: createDeck};
+    function getDecks(email){
+      return $http.get('/decks/localuser/' + email);
+    }
+
+    return {createDeck: createDeck, getDecks: getDecks};
   }]);
 
 
 
-//line end
+//module end
 })();
