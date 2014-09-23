dac328c0ca7809efc0e0a033794afb52c53de5e6
David D. Boling <daviddboling@gmail.com>
2014-09-22 21:38:56 -0500

    Added addOrRemove and Save buttons to deck view, form fields as well.

--- client/views/deck/deck.js
+++ client/views/deck/deck.js
@@ -4,13 +4,21 @@
   var deck = angular.module('knipit');
 
   deck.controller('DeckCtrl', ['$scope', '$routeParams', 'Deck', function($scope, $routeParams, Deck){
-    $scope.title = 'Deck view!';
+    $scope.editMode = false;
     $scope.deck = {};
 
     //inital deck load
     Deck.selectDeck($routeParams.deckId).then(function(res){
       $scope.deck = res.data.deck;
     });
+
+    $scope.editDeck = function(){
+      $scope.editMode = true;
+    };
+
+    $scope.saveDeck = function(){
+      $scope.editMode = false;
+    };
 
   }]);
 
